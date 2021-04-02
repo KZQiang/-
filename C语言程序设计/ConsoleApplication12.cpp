cfb919daab0872f245557319941c4ad79cb8c6d5
KZQiang <1831865161@qq.com>
2021-04-02 17:55:14 +0800

    Add files via upload

---
+++ C语言程序设计/ConsoleApplication12.cpp
@@ -0,0 +1,11 @@
+﻿#include<stdio.h>
+void main()
+{
+	float x, y;
+	scanf_s("%f", &x);
+	if (x < -20)
+	y = 0.55*x + 3.8;
+	else
+		y = 1.6*x - 8;
+	printf("f(%f)=%f\n", x, y);
+}
---
+++ C语言程序设计/ConsoleApplication17.cpp
@@ -0,0 +1,46 @@
+﻿// ConsoleApplication17.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <stdio.h>
+#define N 8
+int main()
+{
+	int a[N], i, m, j, index, midindex = -1;
+	int low, high, mid, temp;
+	printf("\ninput ten numbers:");
+	for (i = 0; i <= N - 1; i++)
+		scanf_s("%d", &a[i]);
+	printf("input search number:\n");
+	scanf_s("%d", &m);
+	for (i = 0; i < N; i++)
+	{
+		index = i;
+		for (j = i; j <= N - 1; j++)
+			if (a[j] < a[index])  index = j;
+		temp = a[i];
+		a[i] = a[index];
+		a[index] = temp;
+	}
+	low = 0;
+	high = N - 1;
+	while (low < high)
+	{
+		mid = (low + high) / 2;
+		if (m > a[mid])
+		low = mid + 1;
+		else if(m < a[mid])
+		high = mid - 1;
+		else
+		{
+			midindex = mid;
+			break;
+		}
+	}
+	printf("output data:\t\n");
+	for (i = 0; i <= N - 1; i++)
+		printf("%d", a[i]);
+	if (midindex != -1)
+		printf("\n midindex=%d", midindex);
+	else
+		printf("\nnot found!");
+}
